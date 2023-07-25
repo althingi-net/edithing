7db595647209e229953adb27a1bad4206f818ff9
Helgi Hrafn Gunnarsson <helgi@binary.is>
2023-07-25 19:30:24 +0000

    Logo needlessly included as an image on webpage and as background in CSS.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import Quill from "quill";
 import "./quill.snow.css";
+import "./style.css";
+import AlthingiNetLogo from "./althingi.net.png";
 
 var quill = new Quill('#editor', {
   debug: 'warning',
   theme: 'snow',
 });
 
+const logo = new Image();
+logo.src = AlthingiNetLogo;
+logo.height = 100;
+logo.weight = 100;
+document.body.append(logo);
