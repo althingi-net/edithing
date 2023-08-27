f93b644b7240acb38ec6022ae85c650747e9948f
Helgi Hrafn Gunnarsson <helgi@binary.is>
2023-08-27 13:44:31 +0000

    Custom Blot: Mark!

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,21 @@ import "./style.css";
 import AlthingiNetLogo from "./althingi.net.png";
 import Constitution from "./1944.33.xml";
 
+let Inline = Quill.import('blots/inline');
+class MarkBlot extends Inline {
+  static blotName = 'mark';
+  static tagName = 'mark';
+};
+Quill.register(MarkBlot);
+
 var quill = new Quill('#editor', {
   debug: 'warning',
   theme: 'snow',
+  modules: {
+    toolbar: {
+      container: "#toolbar"
+    }
+  }
 });
 
 const logo = new Image();
