b9f47c8aa77dac7b81b11194d562551e667cdefd
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-09-09 17:39:39 +0700

    Fix usage of for-of iterator causes problems in CRA by reverting back to ES5

--- src/utils/convertSlateToXml.ts
+++ src/utils/convertSlateToXml.ts
@@ -5,42 +5,42 @@ const convertSlateToXml = (root: Node): string => {
     return convert(root, root);
 }
 
-const convert = (root: Node, value: Node): string => {
-    for (const [node, path] of Node.nodes(value)) {
-        console.log(node, path);
+const convert = (root: Node, node: Node): string => {
+    if (Text.isText(node)) {
+        return node.text;
+    }
 
-        if (Element.isElementType<OrderedList>(node, ElementType.ORDERED_LIST)) {
-            return node.children.map(child => convert(root, child)).join('');
+    if (Element.isElementType<OrderedList>(node, ElementType.ORDERED_LIST)) {
+        return node.children.map(child => convert(root, child)).join('');
+    }
+
+    if (Element.isElementType<ListItem>(node, ElementType.LIST_ITEM)) {
+        const { meta } = node;
+        const { type, nr, nrType, romanNr } = meta;
+        const title = getNodesTitle(node);
+
+        const attributes = [];
+
+        if (nr) {
+            attributes.push(`nr="${nr}"`);
         }
 
-        if (Element.isElementType<ListItem>(node, ElementType.LIST_ITEM)) {
-            const { meta } = node;
-            const { type, nr, nrType, romanNr } = meta;
-            const title = getNodesTitle(node);
-
-            const attributes = [];
-
-            if (nr) {
-                attributes.push(`nr="${nr}"`);
-            }
-
-            if (nrType) {
-                attributes.push(`nr-type="${nrType}"`);
-            }
-
-            if (romanNr) {
-                attributes.push(`roman-nr="${romanNr}"`);
-            }
-
-            const xml = `
-                <${type} ${attributes.join(' ')}>
-                    ${title ? `<nr-title>${title}</nr-title>` : ''}
-                    ${node.children.map(child => convert(root, child)).join('')}
-                </${type}>
-            `;
-
-            return xml;
+        if (nrType) {
+            attributes.push(`nr-type="${nrType}"`);
         }
+
+        if (romanNr) {
+            attributes.push(`roman-nr="${romanNr}"`);
+        }
+
+        const xml = `
+            <${type} ${attributes.join(' ')}>
+                ${title ? `<nr-title>${title}</nr-title>` : ''}
+                ${node.children.map(child => convert(root, child)).join('')}
+            </${type}>
+        `;
+
+        return xml;
     }
 
     return ''
