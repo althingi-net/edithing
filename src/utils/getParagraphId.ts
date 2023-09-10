23599e5fe6b4dd5e40931afd5d8f28d5d2b5433d
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-09-10 17:33:18 +0700

    Implement getParagraphId function to generate unique identifier of a law paragraph

---
+++ src/utils/getParagraphId.ts
@@ -0,0 +1,18 @@
+import { Path, Node, Element } from "slate";
+import { ListItem, ElementType } from "../components/Editor/Slate";
+
+const getParagraphId = (root: Node, path: Path) => {
+    return Array.from(Node.ancestors(root, path))
+        .map(([node]) => {
+            console.log(node);
+            if (Element.isElementType<ListItem>(node, ElementType.LIST_ITEM)) {
+                return `${node.meta.type}-${node.meta.nr}`;
+            }
+
+            return '';
+        })
+        .filter(Boolean)
+        .join('.');
+}
+
+export default getParagraphId;
