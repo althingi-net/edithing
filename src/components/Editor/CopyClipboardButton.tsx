8d5e015681abf642433778519366e77b4bc26768
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-09-18 16:23:15 +0700

    Reset CopyToClipboard state after 500ms

--- src/components/Editor/CopyClipboardButton.tsx
+++ src/components/Editor/CopyClipboardButton.tsx
@@ -1,6 +1,6 @@
 import { CheckOutlined, CopyOutlined } from "@ant-design/icons";
 import { Button } from "antd";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface Props {
     text?: string;
@@ -8,6 +8,18 @@ interface Props {
 
 const CopyClipboardButton: FC<Props> = ({ text }) => {
     const [copied, setCopied] = useState(false);
+
+    useEffect(() => {
+        if (copied) {
+            const timeout = setTimeout(() => {
+                setCopied(false);
+            }, 500);
+
+            return () => {
+                clearTimeout(timeout);
+            }
+        }
+    }, [copied]);
 
     if (!text) {
         return null;
