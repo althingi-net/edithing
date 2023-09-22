06cadc56d031b9f156cdf5fdd8248629032ca730
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-09-22 14:28:35 +0700

    Apply styling to editor content

--- src/components/Editor/plugins/renderElement.tsx
+++ src/components/Editor/plugins/renderElement.tsx
@@ -5,30 +5,35 @@ import { TAGS } from "../Tags";
 export function renderElement({ element, attributes, children }: RenderElementProps) {
     const config = TAGS[element.meta?.type];
 
+    const className = [
+        element.type,
+        element.meta?.type,
+    ].join(' ');
+
     if (config) {
         if (config.display === 'inline' || element.meta?.styleNote === 'inline-with-parent') {
-            return <span {...attributes}>{children}</span>;
+            return <span className={className} {...attributes}>{children}</span>;
         }
 
         if (config.display === 'block') {
-            return <div {...attributes}>{children}</div>;
+            return <div className={className} {...attributes}>{children}</div>;
         }
     }
 
     switch (element.type) {
         case ElementType.ORDERED_LIST:
             // @ts-ignore
-            return <ul {...attributes}>{children}</ul>;
+            return <ul className={className} {...attributes}>{children}</ul>;
         case ElementType.UNORDERED_LIST:
-            return <ul {...attributes}>{children}</ul>;
+            return <ul className={className} {...attributes}>{children}</ul>;
         case ElementType.LIST_ITEM:
             // @ts-ignore
-            return <li value={element.value} {...attributes}>{children}</li>;
+            return <li className={className} value={element.value} {...attributes}>{children}</li>;
         case ElementType.LIST_ITEM_TEXT:
-            return <span {...attributes}>{children}</span>;
+            return <span className={className} {...attributes}>{children}</span>;
         case ElementType.PARAGRAPH:
         default:
-            return <span {...attributes}>{children}</span>;
+            return <span className={className} {...attributes}>{children}</span>;
     }
 }
 
--- src/components/Editor/plugins/renderLeaf.tsx
+++ src/components/Editor/plugins/renderLeaf.tsx
@@ -2,14 +2,15 @@ import { RenderLeafProps } from "slate-react";
 
 export const renderLeaf = ({ attributes, children, leaf }: RenderLeafProps) => {
     if (leaf.title) {
-        return <span {...attributes} style={{ fontWeight: 'bold', marginRight: '10px' }}>{children}</span>;
+        return <span {...attributes} className="title">{children}</span>;
     }
+
     if (leaf.name) {
-        return <span {...attributes} style={{ fontStyle: 'italic', marginRight: '10px' }}>{children}</span>;
+        return <span {...attributes} className="name">{children}</span>;
     }
 
     if (leaf.bold) {
-        return <span {...attributes} style={{ fontWeight: 'bold' }}>{children}</span>;
+        return <span {...attributes} className="bold">{children}</span>;
     }
 
     return <span {...attributes}>{children}</span>;
