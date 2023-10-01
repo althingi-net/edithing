b58bbbfa5e35dd61f97219b966ae0aa5de07028d
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-10-01 21:48:31 +0700

    Print editor content to console when getParagraphId fails

--- src/components/Editor/utils/changelog/getParagraphId.ts
+++ src/components/Editor/utils/changelog/getParagraphId.ts
@@ -2,33 +2,40 @@ import { Node, Path, Text } from "slate";
 import { isListItem } from "../../Slate";
 
 const getParagraphId = (root: Node, path: Path) => {
-    const ids =  Array.from(Node.ancestors(root, path))
-        .map(([node]) => {
-            if (isListItem(node)) {
-                return `${node.meta.type}-${node.meta.nr}`;
-            }
+    try {
+        const ids = Array.from(Node.ancestors(root, path))
+            .map(([node]) => {
+                if (isListItem(node)) {
+                    return `${node.meta.type}-${node.meta.nr}`;
+                }
 
-            return '';
-        })
-        .filter(Boolean);
+                return '';
+            })
+            .filter(Boolean);
 
-    const node = Node.get(root, path);
+        const node = Node.get(root, path);
 
-    if (isListItem(node)) {
-        ids.push(`${node.meta.type}-${node.meta.nr}`);
-    }
-
-    if (Text.isText(node)) {
-        const tag = node.title ? 'title' : node.name ? 'name' : 'sen';
-
-        if (node.nr) {
-            ids.push(`${tag}-${node.nr}`);
-        } else {
-            ids.push(`${tag}`);
+        if (isListItem(node)) {
+            ids.push(`${node.meta.type}-${node.meta.nr}`);
         }
-    }
 
-    return ids.join('.');
+        if (Text.isText(node)) {
+            const tag = node.title ? 'title' : node.name ? 'name' : 'sen';
+
+            if (node.nr) {
+                ids.push(`${tag}-${node.nr}`);
+            } else {
+                ids.push(`${tag}`);
+            }
+        }
+
+        return ids.join('.');
+    } catch (error) {
+        if (process.env.NODE_ENV !== 'production') {
+            console.log('editor', JSON.stringify(root, null, 2));
+        }
+        throw error;
+    }
 }
 
 export default getParagraphId;
