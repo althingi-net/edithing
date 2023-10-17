8ca52b6e8c02b71be5d1d7ac4b0c364daea3167d
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-10-17 17:11:56 +0700

    Disable logging when running tests

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 export const log = (message: string, ...args: any[]) => {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'development') {
         console.log(message, ...args);
     }
 }
