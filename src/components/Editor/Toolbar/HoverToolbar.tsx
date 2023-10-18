7f34a6caddcad01cf704f2f68ea740c86383b0b3
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-10-18 14:51:17 +0700

    Move sentence/title/name buttons from Toolbar to HoverToolbar

--- src/components/Editor/Toolbar/HoverToolbar.tsx
+++ src/components/Editor/Toolbar/HoverToolbar.tsx
@@ -1,26 +1,27 @@
 import { BoldOutlined } from "@ant-design/icons"
-import styles from './HoverToolbar.module.css'
 import { Button, Tooltip } from "antd"
 import { FC, useEffect, useRef } from "react"
-import { Editor, Range, Text } from "slate"
+import { Editor, Range, Text, Transforms } from "slate"
 import { useFocused, useSlate } from "slate-react"
 import Portal from "../../Portal"
-import { format } from "path"
+import styles from './HoverToolbar.module.css'
+import setTitle from "../actions/setTitle"
+import setName from "../actions/setName"
 
-type Marks = keyof Omit<Text, 'text' | 'title' | 'name' | 'nr'>
+type Marks = keyof Omit<Text, 'text' | 'title' | 'name' | 'nr'> | 'title' | 'name' | 'nr';
 
 const HoveringToolbar = () => {
-    const ref = useRef<HTMLDivElement>(null)
-    const editor = useSlate()
-    const inFocus = useFocused()
+    const ref = useRef<HTMLDivElement>(null);
+    const editor = useSlate();
+    const inFocus = useFocused();
 
     useEffect(() => {
-        const el = ref.current
-        const { selection } = editor
-        const domSelection = window.getSelection()
+        const el = ref.current;
+        const { selection } = editor;
+        const domSelection = window.getSelection();
 
         if (!el) {
-            return
+            return;
         }
 
         if (
@@ -30,20 +31,19 @@ const HoveringToolbar = () => {
             Range.isCollapsed(selection) ||
             Editor.string(editor, selection) === ''
         ) {
-            el.removeAttribute('style')
-            return
+            el.removeAttribute('style');
+            return;
         }
 
-        const domRange = domSelection.getRangeAt(0)
-        const rect = domRange.getBoundingClientRect()
-        el.style.opacity = '1'
-        el.style.top = `${rect.top + window.scrollY - el.offsetHeight}px`
+        const domRange = domSelection.getRangeAt(0);
+        const rect = domRange.getBoundingClientRect();
+        el.style.opacity = '1';
+        el.style.top = `${rect.top + window.scrollY - el.offsetHeight}px`;
         el.style.left = `${rect.left +
             window.scrollX -
             el.offsetWidth / 2 +
-            rect.width / 2}px`
-        console.log('huiyd', el.style.top, el.style.left, { rect, scrollX: window.scrollX, scrollY: window.scrollY, offsetWidth: el.offsetWidth, offsetHeight: el.offsetHeight })
-    })
+            rect.width / 2}px`;
+    });
 
     return (
         <Portal>
@@ -56,49 +56,77 @@ const HoveringToolbar = () => {
                 }}
             >
                 <FormatButton format="bold" icon={<BoldOutlined />} />
-                <FormatButton format="bold" icon="T" />
-                <FormatButton format="bold" icon="N" />
+                <FormatButton format="title" icon="T" />
+                <FormatButton format="name" icon="N" />
+                <FormatButton format="nr" icon="S" />
             </div>
         </Portal>
-    )
+    );
 }
 
 const FormatButton: FC<{ format: Marks, icon: any }> = ({ format, icon }) => {
     const editor = useSlate()
+
+    const handleClick = () => {
+        if (format === 'title') {
+            setTitle(editor);
+            return;
+        }
+
+        if (format === 'name') {
+            setName(editor);
+            return;
+        }
+
+        if (format === 'nr') {
+            // split existing sentence and let normalization correct the number across all sentences
+            Transforms.setNodes<Text>(editor, { nr: '' }, { match: Text.isText, split: true });
+            return;
+        }
+
+        toggleMark(editor, format);
+    }
+
     return (
         <Tooltip title={getTooltipText(format)}>
             <Button
                 size="small"
                 className={isMarkActive(editor, format) ? styles.active : undefined}
-                onClick={() => toggleMark(editor, format)}
+                onClick={handleClick}
             >
                 {icon}
             </Button>
         </Tooltip>
-    )
+    );
 }
 
 const getTooltipText = (format: Marks) => {
     switch (format) {
+        case 'title':
+            return `Create a title for this paragraph. Needs to be first text of paragraph`;
+        case 'name':
+            return `Create a name for this paragraph. Needs to be first text of paragraph if there is no title or be right after the title`;
+        case 'nr':
+            return `Format the selected text as its own sentence.`;
         case 'bold':
         default:
-            return `Format selected text with ${format}. Press again to remove formatting.`
+            return `Format selected text with ${format}. Press again to remove formatting.`;
     }
 }
 
 const toggleMark = (editor: Editor, format: Marks) => {
-    const isActive = isMarkActive(editor, format)
+    const isActive = isMarkActive(editor, format);
 
     if (isActive) {
-        Editor.removeMark(editor, format)
+        Editor.removeMark(editor, format);
     } else {
-        Editor.addMark(editor, format, true)
+        Editor.addMark(editor, format, true);
     }
 }
 
 const isMarkActive = (editor: Editor, format: Marks) => {
-    const marks = Editor.marks(editor)
-    return marks ? marks[format] === true : false
+    const marks = Editor.marks(editor);
+    return marks ? marks[format] === true : false;
 }
 
 
--- src/components/Editor/Toolbar/Toolbar.tsx
+++ src/components/Editor/Toolbar/Toolbar.tsx
@@ -1,43 +1,14 @@
-import { Button, Checkbox, Space } from "antd";
-import { Text, Transforms } from "slate";
-import { ReactEditor, useSlateStatic } from "slate-react";
+import { Checkbox, Space } from "antd";
 import useAddEntryButton from "./useAddEntryButton";
 import useHighlightContext from "./useHighlightContext";
-import setTitle from "../actions/setTitle";
-import setName from "../actions/setName";
 
 const Toolbar = () => {
-    const editor = useSlateStatic();
     const addEntryButton = useAddEntryButton();
     const highlight = useHighlightContext();
-    
-    const addTitleMark = () => {
-        setTitle(editor);
-        ReactEditor.focus(editor);
-    };
-
-    const addNameMark = () => {
-        setName(editor);
-        ReactEditor.focus(editor);
-    };
-
-    const addSentenceMark = () => {
-        Transforms.setNodes<Text>(editor, { nr: '' }, { match: Text.isText, split: true});
-        ReactEditor.focus(editor);
-    };
 
     return (
         <Space direction="horizontal" style={{ justifyContent: 'center', marginBottom: '10px', width: '100%' }}>
             {addEntryButton}
-            <Button type="primary" onClick={addTitleMark}>
-                Title
-            </Button>
-            <Button type="primary" onClick={addNameMark}>
-                Name
-            </Button>
-            <Button type="primary" onClick={addSentenceMark}>
-                Sentence
-            </Button>
             <Checkbox checked={highlight?.isHighlighted} onChange={(event) => highlight?.setHighlighted(event.target.checked)}>
                 Highlight sentences
             </Checkbox>
