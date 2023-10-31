9aa0e95be923f908ed5c44554fae38bb70c596bd
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-10-31 15:35:38 +0700

    Fix: eslint error

--- src/components/Editor/plugins/withLawParagraphs.ts
+++ src/components/Editor/plugins/withLawParagraphs.ts
@@ -3,7 +3,6 @@ import { log } from '../../../logger';
 import { isList, isListItem, isListItemText } from '../Slate';
 import createListItemMeta from '../utils/slate/createListItemMeta';
 import createListMeta from '../utils/slate/createListMeta';
-import getParentListItem from '../utils/slate/getParentListItem';
 import incrementFollowingSiblings from '../utils/slate/incrementFollowingSiblings';
 import setListItemMeta from '../utils/slate/setListItemMeta';
 import setMeta from '../utils/slate/setMeta';
@@ -51,84 +50,84 @@ const normalizeMissingMeta = (editor: Editor, entry: NodeEntry) => {
     return false;
 };
 
-const normalizeMovedListItem = (editor: Editor, entry: NodeEntry) => {
-    const [node, path] = entry;
+// const normalizeMovedListItem = (editor: Editor, entry: NodeEntry) => {
+//     const [node, path] = entry;
 
-    const grandParent = getParentListItem(editor, path);
-    const isMovedDownListItem = isListItem(node) && grandParent && grandParent[0].meta.type === node.meta?.type;
+//     const grandParent = getParentListItem(editor, path);
+//     const isMovedDownListItem = isListItem(node) && grandParent && grandParent[0].meta.type === node.meta?.type;
 
-    if (isMovedDownListItem) {
-        const meta = createListItemMeta(editor, path);
-        log('update meta of moved list item', { node, path, meta });
-        setListItemMeta(editor, node, path, meta);
-        return true;
-    }
+//     if (isMovedDownListItem) {
+//         const meta = createListItemMeta(editor, path);
+//         log('update meta of moved list item', { node, path, meta });
+//         setListItemMeta(editor, node, path, meta);
+//         return true;
+//     }
 
-    // if (isListItem(node) && node['meta']) {
-    //     const [parent] = Editor.parent(editor, path);
-    //     const newNr = `${path[path.length - 1] + 1}`;
+//     // if (isListItem(node) && node['meta']) {
+//     //     const [parent] = Editor.parent(editor, path);
+//     //     const newNr = `${path[path.length - 1] + 1}`;
 
-    //     if (
-    //         parent &&
-    //         isList(parent) &&
-    //         (!parent.meta || parent.meta.type === node.meta.type)
-    //     ) {
-    //         return false;
-    //     }
+//     //     if (
+//     //         parent &&
+//     //         isList(parent) &&
+//     //         (!parent.meta || parent.meta.type === node.meta.type)
+//     //     ) {
+//     //         return false;
+//     //     }
 
-    //     // Wrap missing List around ListItem
-    //     if (!parent || !isList(parent)) {
-    //         Transforms.wrapNodes(
-    //             editor,
-    //             createList(node.meta.type),
-    //             { at: path },
-    //         )
-    //         log('wrap list item', {node, path, parent})
-    //         return true;
-    //     }
+//     //     // Wrap missing List around ListItem
+//     //     if (!parent || !isList(parent)) {
+//     //         Transforms.wrapNodes(
+//     //             editor,
+//     //             createList(node.meta.type),
+//     //             { at: path },
+//     //         )
+//     //         log('wrap list item', {node, path, parent})
+//     //         return true;
+//     //     }
 
-    //     const sibling = getPreviousSibling(editor, path);
+//     //     const sibling = getPreviousSibling(editor, path);
 
-    //     // copy sibling meta
-    //     if (sibling && isListItem(sibling)) {
-    //         const meta = createListItemMetaFromSibling(sibling);
-    //         log('set list item meta via sibling', {node, path, sibling, from: node.meta, to: meta});
+//     //     // copy sibling meta
+//     //     if (sibling && isListItem(sibling)) {
+//     //         const meta = createListItemMetaFromSibling(sibling);
+//     //         log('set list item meta via sibling', {node, path, sibling, from: node.meta, to: meta});
 
-    //         setListItemMeta(editor, node, path, meta);
+//     //         setListItemMeta(editor, node, path, meta);
 
-    //         return true;
+//     //         return true;
 
-    //     // copy parent meta
-    //     } else if (parent && isList(parent) && parent.meta?.type !== node.meta.type) {
-    //         const meta: ListItemMeta = {
-    //             ...node.meta,
-    //             nr: newNr,
-    //             type: parent.meta?.type || node.meta.type, // TODO: check if this is correct
-    //         }
-    //         log('set list item meta via parent', {node, path, parent, from: node.meta, to: meta});
+//     //     // copy parent meta
+//     //     } else if (parent && isList(parent) && parent.meta?.type !== node.meta.type) {
+//     //         const meta: ListItemMeta = {
+//     //             ...node.meta,
+//     //             nr: newNr,
+//     //             type: parent.meta?.type || node.meta.type, // TODO: check if this is correct
+//     //         }
+//     //         log('set list item meta via parent', {node, path, parent, from: node.meta, to: meta});
 
-    //         setListItemMeta(editor, node, path, meta);
+//     //         setListItemMeta(editor, node, path, meta);
 
-    //         return true;
+//     //         return true;
 
-    //     // default to CHAPTER
-    //     } else {
-    //         const newNr = path[path.length - 1] + 1;
-    //         const meta: ListItemMeta = {
-    //             ...node.meta,
-    //             nr: `${newNr}`,
-    //             romanNr: convertRomanNumber(newNr),
-    //             nrType: 'roman',
-    //             type: MetaType.CHAPTER,
-    //         }
-    //         log('set list item meta via default', {node, path, parent, from: node.meta, to: meta});
+//     //     // default to CHAPTER
+//     //     } else {
+//     //         const newNr = path[path.length - 1] + 1;
+//     //         const meta: ListItemMeta = {
+//     //             ...node.meta,
+//     //             nr: `${newNr}`,
+//     //             romanNr: convertRomanNumber(newNr),
+//     //             nrType: 'roman',
+//     //             type: MetaType.CHAPTER,
+//     //         }
+//     //         log('set list item meta via default', {node, path, parent, from: node.meta, to: meta});
 
-    //         setListItemMeta(editor, node, path, meta);
+//     //         setListItemMeta(editor, node, path, meta);
 
-    //         return true;
-    //     }
-    // }
-};
+//     //         return true;
+//     //     }
+//     // }
+// };
 
 const normalizeSentences = (editor: Editor, entry: NodeEntry) => {
     const [node, path] = entry;
@@ -161,75 +160,75 @@ const normalizeSentences = (editor: Editor, entry: NodeEntry) => {
     return false;
 };
 
-const enforceTitleNameSenLayout = (editor: Editor, entry: NodeEntry) => {
-    const [node, path] = entry;
+// const enforceTitleNameSenLayout = (editor: Editor, entry: NodeEntry) => {
+//     const [node, path] = entry;
 
-    if (!isListItemText(node)) {
-        return false;
-    }
+//     if (!isListItemText(node)) {
+//         return false;
+//     }
 
-    const listItem = Node.parent(editor, path);
+//     const listItem = Node.parent(editor, path);
 
-    if (!isListItem(listItem) || !listItem.meta) {
-        return false;
-    }
+//     if (!isListItem(listItem) || !listItem.meta) {
+//         return false;
+//     }
 
-    // remove empty text node in front of others
-    if (node.children.length > 1 && node.children[0].text === '') {
-        const at = [...path, 0];
-        Transforms.removeNodes(editor, { at });
-        return true;
-    }
+//     // remove empty text node in front of others
+//     if (node.children.length > 1 && node.children[0].text === '') {
+//         const at = [...path, 0];
+//         Transforms.removeNodes(editor, { at });
+//         return true;
+//     }
 
-    const { meta: { title, name } } = listItem;
-    // let hasChanges = false;
+//     const { meta: { title, name } } = listItem;
+//     // let hasChanges = false;
 
-    // Editor.withoutNormalizing(editor, () => {
-    node.children.forEach((child, index) => {
-        const text = child.text;
-        const hasTitle = title;
-        const hasName = name;
+//     // Editor.withoutNormalizing(editor, () => {
+//     node.children.forEach((child, index) => {
+//         const text = child.text;
+//         const hasTitle = title;
+//         const hasName = name;
 
-        if (hasTitle) {
-            if (index === 0 && (!child.title || child.name || child.nr)) {
-                const at = [...path, index];
-                log('enforce title', { node, path, child, index, title });
-                Transforms.removeNodes(editor, { at });
-                Transforms.insertNodes<Text>(editor, { text, title: true }, { at });
-                return true;
-            }
+//         if (hasTitle) {
+//             if (index === 0 && (!child.title || child.name || child.nr)) {
+//                 const at = [...path, index];
+//                 log('enforce title', { node, path, child, index, title });
+//                 Transforms.removeNodes(editor, { at });
+//                 Transforms.insertNodes<Text>(editor, { text, title: true }, { at });
+//                 return true;
+//             }
 
-            if (index === 1 && hasName && (!child.name || child.title || child.nr)) {
-                const at = [...path, index];
-                log('enforce name', { node, path, child, index, name });
-                Transforms.removeNodes(editor, { at });
-                Transforms.insertNodes<Text>(editor, { text, name: true }, { at });
-                return true;
-            }
-        } else {
-            if (index === 0 && hasName && (!child.name || child.title || child.nr)) {
-                const at = [...path, index];
-                log('enforce name', { node, path, child, index, name });
-                Transforms.removeNodes(editor, { at });
-                Transforms.insertNodes<Text>(editor, { text, name: true }, { at });
-                return true;
-            }
-        }
+//             if (index === 1 && hasName && (!child.name || child.title || child.nr)) {
+//                 const at = [...path, index];
+//                 log('enforce name', { node, path, child, index, name });
+//                 Transforms.removeNodes(editor, { at });
+//                 Transforms.insertNodes<Text>(editor, { text, name: true }, { at });
+//                 return true;
+//             }
+//         } else {
+//             if (index === 0 && hasName && (!child.name || child.title || child.nr)) {
+//                 const at = [...path, index];
+//                 log('enforce name', { node, path, child, index, name });
+//                 Transforms.removeNodes(editor, { at });
+//                 Transforms.insertNodes<Text>(editor, { text, name: true }, { at });
+//                 return true;
+//             }
+//         }
 
-        const senStartIndex = hasTitle ? hasName ? 2 : 1 : hasName ? 1 : 0;
-        const newNr = `${index - senStartIndex + 1}`;
+//         const senStartIndex = hasTitle ? hasName ? 2 : 1 : hasName ? 1 : 0;
+//         const newNr = `${index - senStartIndex + 1}`;
 
-        if (index >= senStartIndex && (!child.nr || child.title || child.name || child.nr !== newNr)) {
-            const at = [...path, index];
-            log('enforce sen', { node, path, child, index, senStartIndex, newNr });
-            Transforms.removeNodes(editor, { at });
-            Transforms.insertNodes<Text>(editor, { text, nr: newNr }, { at });
-            return true;
-        }
-    });
-    // });
+//         if (index >= senStartIndex && (!child.nr || child.title || child.name || child.nr !== newNr)) {
+//             const at = [...path, index];
+//             log('enforce sen', { node, path, child, index, senStartIndex, newNr });
+//             Transforms.removeNodes(editor, { at });
+//             Transforms.insertNodes<Text>(editor, { text, nr: newNr }, { at });
+//             return true;
+//         }
+//     });
+//     // });
 
-    return false;
-};
+//     return false;
+// };
 
 export default withLawParagraphs;
