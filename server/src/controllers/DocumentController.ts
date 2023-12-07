b0014fd8446f4eb0cce4ce58cb0fdc448314328b
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-12-07 13:36:40 +0700

    Add better description to api documentation for Document endpoints

--- server/src/controllers/DocumentController.ts
+++ server/src/controllers/DocumentController.ts
@@ -1,5 +1,5 @@
 import { Get, JsonController, Param } from 'routing-controllers';
-import { ResponseSchema } from 'routing-controllers-openapi';
+import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
 import Document from '../entities/Document';
 import User from '../entities/User';
 import downloadFile from '../integration/github/downloadFile';
@@ -10,8 +10,9 @@ import getLawEntries from '../integration/github/getLawEntries';
 class DocumentController {
 
     /**
-     * Returns a list of all documents stored in a xml file on github.
+     * Get a list of all documents stored in a xml file on github.
      */
+    @OpenAPI({ summary: 'Get a list of all documents stored in a xml file on github.' })
     @Get('/document')
     @ResponseSchema(User, { isArray: true })
     async getAll() {
@@ -19,10 +20,11 @@ class DocumentController {
     }
 
     /**
-     * Returns a document from the database if it exists, otherwise it downloads it from github and saves it to the database.
+     * Get a document from the database if it exists, otherwise it downloads it from github and saves it to the database.
      * @param path The path to the document on github. Example: `data/xml/1944.033.xml`
      * @returns The document.
      */
+    @OpenAPI({ summary: 'Get a document from the database if it exists, otherwise it downloads it from github and saves it to the database.' })
     @Get('/document/:path')
     async get(@Param('path') path: string) {
         let document = await Document.findOneBy({ path });
