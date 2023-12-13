440cce6987ad152dd6f93908ce64d7c1ba281724
Helgi Hrafn Gunnarsson <helgi@binary.is>
2023-12-13 14:50:33 +0000

    Database config moved to environment settings. Defaults in .env.sample.

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,9 +4,12 @@ const database: DataSourceOptions = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: 'root_password',
-    database: process.env.NODE_ENV === 'test' ? 'test' : 'app',
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database:
+        process.env.NODE_ENV === "test"
+            ? process.env.DATABASE_NAME + "_test"
+            : process.env.DATABASE_NAME,
     synchronize: process.env.NODE_ENV !== 'production',
     entities: ['**/entities/!(*.test).ts'],
     logging: true,
