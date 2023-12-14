994bbba466b65ca4079d8b29beb34d55c6a0f130
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-12-14 14:24:37 +0700

    Add aurtomatic build of sdk at server startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,18 @@
-import 'reflect-metadata';
+import { exec } from 'child_process';
 import 'dotenv/config';
-import server from './config/server';
+import 'reflect-metadata';
 import app from './app';
+import server from './config/server';
 import { initConnection } from './integration/database/connection';
 
 // Start the server
 (async () => {
     await initConnection();
     await app.listen(server.port);
-
     console.log(`🚀 Server is running at ${server.host}`);
+
+    if (process.env.NODE_ENV !== 'production') {
+        console.log('Generating Client SDK...');
+        exec('npm run build:sdk');
+    }
 })();
