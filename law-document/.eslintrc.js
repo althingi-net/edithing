b8055268e3a20051d1def8f6bfd24ef4b0f25ae4
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2023-12-26 14:23:00 +0700

    Setup jest, eslint and vscode for law-document

---
+++ law-document/.eslintrc.js
@@ -0,0 +1,110 @@
+module.exports = {
+    'env': {
+        'node': true,
+        'es2021': true
+    },
+    ignorePatterns: [
+        'node_modules/',
+        'dist/'
+    ],
+    'extends': [
+        'eslint:recommended',
+        'plugin:import/recommended',
+    ],'overrides': [{
+        // Config files
+        'env': {
+            'node': true
+        },
+        'files': [
+            '.eslintrc.{js,cjs}',
+            'jest.config.{js,cjs}'
+        ],
+        'parserOptions': {
+            'sourceType': 'script'
+        },
+    }, {
+        // Typescript
+        'extends': [
+            'plugin:import/typescript',
+            'plugin:@typescript-eslint/eslint-recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        ],
+        'plugins': [
+            '@typescript-eslint',
+        ],
+        'parser': '@typescript-eslint/parser',
+        'parserOptions': {
+            'ecmaVersion': 'latest',
+            'sourceType': 'module',
+            'project': './tsconfig.json',
+            'tsconfigRootDir': __dirname,
+        },
+        'files': ['src/**/*.ts?(x)'],
+        'rules': {
+            // Developer experience
+            '@typescript-eslint/no-unnecessary-condition': ['error', {
+                'allowConstantLoopConditions': true
+            }],
+            '@typescript-eslint/no-unused-vars': ['error', { 'ignoreRestSiblings': true }],
+            
+            // Code quality
+            'import/no-duplicates': 'error',
+            'import/no-named-as-default': 'error',
+            // TODO: Remove these rules
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-misused-promises': 'off',
+        },
+    }, {
+        // Tests
+        'env': {
+            'jest': true
+        },
+        'files': [
+            '**/__tests__/**/*.[jt]s?(x)',
+            '**/?(*.)+(spec|test).[tj]s?(x)'
+        ],
+        'parser': '@typescript-eslint/parser',
+        'parserOptions': {
+            'ecmaVersion': 'latest',
+            'sourceType': 'module',
+            'project': './tsconfig.json',
+            'tsconfigRootDir': __dirname,
+        },
+        'plugins': [
+            'jest',
+        ],
+        'extends': [
+            'plugin:jest/recommended',
+        ],
+    }],
+    'rules': {
+        // Formatting
+        'indent': [
+            'error',
+            4,
+            { 'SwitchCase': 1, 'ignoredNodes': ['PropertyDefinition']}
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        // 'object-curly-spacing': [
+        //     'error',
+        //     'always',
+        //     { 'arraysInObjects': false, 'objectsInObjects': false }
+        // ],
+    },
+};
