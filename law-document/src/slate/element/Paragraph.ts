1aa51dd3c736edd3452e9d7638b17df9790cc52d
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-01-07 10:40:56 +0700

    Change type of paragraph children to Descendant to avoid union type conflict

--- law-document/src/slate/element/Paragraph.ts
+++ law-document/src/slate/element/Paragraph.ts
@@ -1,7 +1,7 @@
-import { Text } from 'slate';
+import { Descendant } from 'slate';
 import { ElementType } from '../Slate';
 
 export interface Paragraph {
     type: ElementType.PARAGRAPH;
-    children: Text[];
+    children: Descendant[];
 }
