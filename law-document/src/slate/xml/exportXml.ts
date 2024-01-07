ae4332cae4243eca45f5f4a22cfcefa35ef27fe6
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-01-07 10:41:41 +0700

    Handle html content of minister-clause tag in export

--- law-document/src/slate/xml/exportXml.ts
+++ law-document/src/slate/xml/exportXml.ts
@@ -1,21 +1,25 @@
 import { Editor, Element, Node, Path, Text } from 'slate';
 import xmlFormat from 'xml-formatter';
+import { DocumentMetaElement, isDocumentMeta } from '../element/DocumentMetaElement';
 import { isList } from '../element/List';
 import { isListItem } from '../element/ListItem';
 import { isListItemText } from '../element/ListItemText';
-import { isDocumentMeta, DocumentMetaElement } from '../element/DocumentMetaElement';
 
-export const exportXml = (editor: Editor, addHeader = false): string => {
+export const exportXml = (root: Node, addHeader = false): string => {
+    if (Text.isText(root)) {
+        throw new Error('exportXml: root is Text');
+    }
+
     const xml = [];
 
     if (addHeader) {
         xml.push('<?xml version="1.0" encoding="utf-8"?>');
     }
 
-    const slateXml = convertSlate(editor, editor, []);
+    const slateXml = convertSlate(root, root, []);
 
-    if (editor.children.length > 0 && isDocumentMeta(editor.children[0])) {
-        const documentMetaElement = editor.children[0];
+    if (root.children.length > 0 && isDocumentMeta(root.children[0])) {
+        const documentMetaElement = root.children[0];
 
         xml.push(convertDocumentMetaToXml(documentMetaElement, slateXml));
     } else {
@@ -39,15 +43,27 @@ const convertDocumentMetaToXml = (element: DocumentMetaElement, children: string
                 <num>${nr}</num>
                 <original>${original}</original>
             </num-and-date>
-            <minister-clause>${ministerClause}</minister-clause>
+            <minister-clause>${escapeHtmlSpecialChars(ministerClause)}</minister-clause>
             ${children}
         </law>
     `;
 };
 
-const convertSlate = (editor: Editor, node: Node, path: Path): string => {
+const escapeHtmlSpecialChars = (str: string) => {
+    const map: Record<string, string> = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '\'': '&#39;' // ' -> &apos; for XML only
+    };
+    
+    return str.replace(/[&<>"']/g, function(m) { return map[m]; });
+};
+
+const convertSlate = (root: Node, node: Node, path: Path): string => {
     if (isList(node) || Editor.isEditor(node)) {
-        return node.children.map((child, index) => convertSlate(editor, child, [...path, index])).join('');
+        return node.children.map((child, index) => convertSlate(root, child, [...path, index])).join('');
     }
 
     if (isListItem(node)) {
@@ -98,7 +114,7 @@ const convertSlate = (editor: Editor, node: Node, path: Path): string => {
                 ${title ? `<nr-title>${title}</nr-title>` : ''}
                 ${name ? `<name>${name}</name>` : ''}
                 ${sentences.map((sentence, index) => `<sen nr="${index + 1}">${sentence.text}</sen>`).join('')}
-                ${otherChildren.map((child, index) => convertSlate(editor, child, [...path, index])).join('')}
+                ${otherChildren.map((child, index) => convertSlate(root, child, [...path, index])).join('')}
             </${type}>
         `;
 
@@ -106,7 +122,7 @@ const convertSlate = (editor: Editor, node: Node, path: Path): string => {
     }
 
     if (Element.isElement(node)) {
-        return node.children.map((child, index) => convertSlate(editor, child, [...path, index])).join('');
+        return node.children.map((child, index) => convertSlate(root, child, [...path, index])).join('');
     }
 
     return '';
