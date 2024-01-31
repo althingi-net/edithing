012b574d263896234c72c075de9afaed44ee344b
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-01-31 14:04:44 +0800

    Move router context to the root of the app to allow access above routes

--- client/src/features/App/App.tsx
+++ client/src/features/App/App.tsx
@@ -1,26 +1,64 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Layout } from 'antd';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import BillPage from '../../pages/BillPage';
+import BillsPage from '../../pages/BillsPage';
+import LawListPage from '../../pages/LawListPage';
+import LawPage from '../../pages/LawPage';
 import { LawListContextProvider } from '../Documents/useLawListContext';
 import { HightlightContextProvider } from '../Editor/Toolbar/useHighlightContext';
-import Router from './Router';
+import { BlockNavigationProvider } from './useBlockNavigation';
 import { LanguageContextProvider } from './useLanguageContext';
-import { ThemeContextProvider } from './useThemeContext';
 import { SessionContextProvider } from './useSessionContext';
+import { ThemeContextProvider } from './useThemeContext';
+
+const router = createBrowserRouter([{
+    element: (
+        <SessionContextProvider>
+            <BlockNavigationProvider>
+                <ThemeContextProvider>
+                    <LanguageContextProvider>
+                        <HightlightContextProvider>
+                            <LawListContextProvider>
+                                <Layout style={{ height: '100vh', overflow: 'auto', display: 'flex', flexDirection: 'column' }}>
+                                    <Outlet />
+                                </Layout>
+                            </LawListContextProvider>
+                        </HightlightContextProvider>
+                    </LanguageContextProvider>
+                </ThemeContextProvider>
+            </BlockNavigationProvider>
+        </SessionContextProvider>
+    ),
+    children: [
+        {
+            path: '/',
+            element: <LawListPage/>,
+        }, {
+            path: '/law/:identifier',
+            element: <LawPage/>,
+        }, {
+            path: '/bills',
+            element: <BillsPage/>,
+        }, {
+            path: '/bill/:id',
+            element: <BillPage/>,
+        }, {
+            path: '/bill/:id/document/:identifier',
+            element: <BillPage/>,
+        },
+    ],
+}]);
+
+// @ts-ignore
+if (import.meta.hot) {
+    // @ts-ignore
+    import.meta.hot.dispose(() => router.dispose());
+}
 
 function App() {
     return (
-        <SessionContextProvider>
-            <ThemeContextProvider>
-                <LanguageContextProvider>
-                    <HightlightContextProvider>
-                        <LawListContextProvider>
-                            <Layout style={{ height: '100vh', overflow: 'auto', display: 'flex', flexDirection: 'column' }}>
-                                <Router />
-                            </Layout>
-                        </LawListContextProvider>
-                    </HightlightContextProvider>
-                </LanguageContextProvider>
-            </ThemeContextProvider>
-        </SessionContextProvider>
+        <RouterProvider router={router} />
     );
 }
 
