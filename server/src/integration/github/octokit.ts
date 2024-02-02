a2d764b76a7c0a6cf870b9a02c6cf5c7d5ac987d
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-02-02 16:59:25 +0800

    Remove github token check on app start

--- server/src/integration/github/octokit.ts
+++ server/src/integration/github/octokit.ts
@@ -1,10 +1,6 @@
 import { Octokit } from 'octokit';
 import github from '../../config/github';
 
-if (!github.token) {
-    throw new Error('Missing github token. Please create a .env file with a GITHUB_TOKEN variable.');
-}
-
 const octokit = new Octokit({
     auth: github.token,
 });
