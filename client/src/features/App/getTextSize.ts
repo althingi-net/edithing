79cb80cc459cdaffb836bb39a0c9bcc470027d3e
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-02-09 03:19:09 -0300

    Improve performance of DocumentsList

---
+++ client/src/features/App/getTextSize.ts
@@ -0,0 +1,18 @@
+const getTextSize = (text: string) => {
+    const element = document.createElement('canvas');
+    const context = element.getContext('2d');
+    // context.font = font;
+
+    if (!context) {
+        return { width: 0, height: 0 };
+    }
+
+    const size = {
+        'width': context.measureText(text).width,
+        'height': parseInt(context.font)
+    };
+
+    return size;
+}
+
+export default getTextSize;
--- client/src/features/Bills/DocumentsList.tsx
+++ client/src/features/Bills/DocumentsList.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
+import { VariableSizeList } from 'react-window';
 import { FileAddOutlined } from '@ant-design/icons';
 import { Tooltip } from 'antd';
 import { GithubFile } from 'client-sdk';
 import { FC } from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
 import useLanguageContext from '../App/useLanguageContext';
 import formatIdentifier from '../Documents/formatIdentifier';
+import Loader from '../App/Loader';
+import getTextSize from '../App/getTextSize';
 
 interface Props {
     documents: GithubFile[] | undefined,
@@ -16,37 +20,61 @@ interface Props {
 const DocumentsList: FC<Props> = ({ documents, onAddDocument, setSelected, selected }) => {
     const { t } = useLanguageContext();
 
+    if (!documents) return (
+        <Loader />
+    );
+
+    const getItemSize = (index: number) => {
+        const textBoxWidth = 183;
+        const document = documents[index];
+        const size = getTextSize(document.name);
+        const lines = Math.ceil(size.width / textBoxWidth);
+        return 15 + 20 + lines * 15;
+    };
+
     return (
         <>
-            {documents?.map(doc => 
-                <div
-                    className={['list-item', selected === doc.identifier ? 'selected' : ''].join(' ')}
-                    key={doc.identifier}
-                    role="button"
-                    tabIndex={0}
-                    onClick={() => setSelected(doc.identifier)}
-                >
-                    <div className='infos'>
-                        <div className='item-identifier'>{formatIdentifier(doc.identifier)}</div>
-                        <div className='item-title'>{doc.name}</div>
-                    </div>
-                    <div className='item-actions'>
-                        <Tooltip title={t('Add document to bill and start editing')}>
+            <AutoSizer>
+                {({ height, width }) => (
+                    <VariableSizeList
+                        itemCount={documents.length}
+                        itemSize={getItemSize}
+                        height={height}
+                        width={width}
+                    >
+                        {({ index, style }) => (
                             <div
-                                className='item-action add'
-                                role='button'
+                                style={style}
+                                className={['list-item', selected === documents[index].identifier ? 'selected' : ''].join(' ')}
+                                key={documents[index].identifier}
+                                role="button"
                                 tabIndex={0}
-                                onClick={(event) => {
-                                    event.stopPropagation();
-                                    onAddDocument(doc.identifier);
-                                }}
+                                onClick={() => setSelected(documents[index].identifier)}
                             >
-                                <FileAddOutlined />
+                                <div className='infos'>
+                                    <div className='item-identifier'>{formatIdentifier(documents[index].identifier)}</div>
+                                    <div className='item-title'>{documents[index].name}</div>
+                                </div>
+                                <div className='item-actions'>
+                                    <Tooltip title={t('Add document to bill and start editing')}>
+                                        <div
+                                            className='item-action add'
+                                            role='button'
+                                            tabIndex={0}
+                                            onClick={(event) => {
+                                                event.stopPropagation();
+                                                onAddDocument(documents[index].identifier);
+                                            }}
+                                        >
+                                            <FileAddOutlined />
+                                        </div>
+                                    </Tooltip>
+                                </div>
                             </div>
-                        </Tooltip>
-                    </div>
-                </div>
-            )}
+                        )}
+                    </VariableSizeList>
+                )}
+            </AutoSizer>
         </>
     );
 };
