45d404ce30933858021f9fa6226887ec27d3074a
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-02-19 19:11:02 +0800

    Implement Bill XML export

---
+++ server/src/services/BillService/exportBillXml.test.ts
@@ -0,0 +1,95 @@
+import { createEditorWithPlugins } from 'law-document';
+
+const setupEditor = (document: any[]) => {
+    const editor = createEditorWithPlugins(); 
+    editor.children = document;
+
+    const originalDocument = document;
+
+    return { editor, originalDocument };
+};
+
+test.todo('export added', () => {
+
+    // const { editor, originalDocument } = setupEditor([
+    //     createList(MetaType.CHAPTER, {}, [
+    //         createListItem(MetaType.CHAPTER, '1', { title: '1. Chapter' }, [
+    //             createList(MetaType.ART, {}, [
+    //                 createListItem(MetaType.ART, '1', { text: 'some text' }),
+    //             ]),
+    //         ]),
+    //     ]),
+    // ]);
+
+    // Transforms.insertText(editor, 'sd', { at: { path: [0, 0, 0, 0], offset: 10 } });
+
+    // const bill: Bill = {
+    //     title: 'Title',
+    // };
+    // const billDocuments: BillDocument[] = [{
+    //     originalXml: JSON.stringify([]),
+    //     content: JSON.stringify([]),
+    //     events: JSON.stringify([]),
+    //     identifier: 'Chapter-1',
+    //     title: 'Chapter 1',
+    // }];
+
+    // const changelog: Changelog[] = [{
+    //     type: 'added',
+    //     id: 'Chapter-1.Art-2',
+    //     text: 'New Paragraph',
+    // }];
+
+    // expect(exportBillXml(changelog)).toBe(xmlFormat(`
+    //     <changes>
+    //         <art nr="1">
+    //             <subart nr="1">
+    //                 <sen nr="1" change-type="added" origin-id="Chapter-1.Art-2">
+    //                     New Paragraph
+    //                 </sen>
+    //             </subart>
+    //         </art>
+    //     </changes>
+    // `));
+});
+
+test.todo('export removed', () => {
+    // const changelog: Changelog[] = [{
+    //     type: 'deleted',
+    //     id: 'Chapter-1.Art-2',
+    //     text: 'New Paragraph',
+    // }];
+
+    // expect(exportBillXml(changelog)).toBe(xmlFormat(`
+    //     <changes>
+    //         <art nr="1">
+    //             <subart nr="1">
+    //                 <sen nr="1" change-type="deleted" origin-id="Chapter-1.Art-2">
+    //                     New Paragraph
+    //                 </sen>
+    //             </subart>
+    //         </art>
+    //     </changes>
+    // `));
+});
+
+test.todo('export changed', () => {
+    // const changelog: Changelog[] = [{
+    //     type: 'changed',
+    //     id: 'Chapter-1.Art-2',
+    //     text: 'New Paragraph',
+    // }];
+
+    // expect(exportBillXml(changelog)).toBe(xmlFormat(`
+    //     <changes>
+    //         <art nr="1">
+    //             <subart nr="1">
+    //                 <sen nr="1" change-type="changed" origin-id="Chapter-1.Art-2">
+    //                     New Paragraph
+    //                 </sen>
+    //             </subart>
+    //         </art>
+    //     </changes>
+    // `));
+});
+    
---
+++ server/src/services/BillService/exportBillXml.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Event, compareDocuments, formatIdentifier } from 'law-document';
+import { LawEditor, MetaType } from 'law-document/src/slate/Slate';
+import { Changelog } from 'law-document/src/slate/changelog/Changelog';
+import { groupChangesByArticle } from 'law-document/src/slate/changelog/groupChangesByArticle';
+// import { Descendant } from 'slate';
+import xmlFormat from 'xml-formatter';
+import Bill from '../../entities/Bill';
+import BillDocument from '../../entities/BillDocument';
+
+export const exportBillXml = (bill: Bill, billDocuments: BillDocument[]): string => {
+    let articleCountOffset = 0;
+
+    const chapters = billDocuments.map((document, index) => {
+        const { xml, articleCount } = parseDocument(document, index + 1, articleCountOffset);
+        
+        articleCountOffset = articleCount;
+        return xml;
+    });
+
+    return xmlFormat(`
+        <bill>
+            <title>
+                ${bill.title}
+            </title>
+            ${chapters.join('\n')}
+        </bill>
+    `);
+};
+
+const parseDocument = (document: BillDocument, chapterNr: number, articleCountOffset: number) => {
+    const originalContent = JSON.parse(document.originalXml) as any[];
+    const newContent = JSON.parse(document.content) as any[];
+    const events = JSON.parse(document.events) as Event[];
+    
+    const changelog = compareDocuments(
+        {
+            children: newContent,
+            events,
+        } as LawEditor, 
+        originalContent
+    );
+    const articles = createChangelogXml(changelog, articleCountOffset);
+
+    return {
+        xml: `
+            <${MetaType.CHAPTER} nr="${chapterNr}" identifier="${document.identifier}">
+                <title>
+                    Breyting á ${document.title}, no. ${formatIdentifier(document.identifier)}.
+                </title>
+                ${articles.join('\n')}
+            </${MetaType.CHAPTER}>
+        `,
+        articleCount: articles.length + articleCountOffset,
+    };
+};
+
+const createChangelogXml = (changelog: Changelog[], articleCountOffset: number) => {
+    const groupedChanges = groupChangesByArticle(changelog);
+    const articles = Object.entries(groupedChanges).map(([, changes], index) => {
+        return `
+            <${MetaType.ART} nr="${index + 1 + articleCountOffset}">
+                ${changes.map((change, index) => `
+                    <${MetaType.SUBART} nr="${index + 1}">
+                        <${MetaType.SEN} nr="1" change-type="${change.type}" origin-id="${change.id}">
+                            ${parseChange(change)}
+                        </${MetaType.SEN}>
+                    </${MetaType.SUBART}>
+                `).join('\n')}
+            </${MetaType.ART}>
+        `;
+    });
+
+    return articles;
+};
+
+const parseChange = (change: Changelog) => {
+    return change.text;
+};
