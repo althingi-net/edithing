79894f8119252b21be45d0d59bb716cd7a6e3e5c
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-05-13 19:00:13 +0700

    Setup Storybook

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
         'plugin:import/recommended',
+        'plugin:storybook/recommended',
     ],
     'plugins': [
         'react',
