981b8871b96745d9b70007c4c6e64103efbf2043
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-05-16 14:23:49 +0700

    Upgrade Storybook to 8.1

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -11,6 +11,7 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
@@ -18,12 +19,16 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("storybook-addon-deep-controls"),
   ],
+
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
