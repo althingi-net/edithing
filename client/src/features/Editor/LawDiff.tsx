1adedfc551e5d7be6acc449b475afc93e932f477
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-08-27 16:27:42 +0200

    Replace changelog text-diff with xml-diff library

--- client/src/features/Editor/LawDiff.tsx
+++ client/src/features/Editor/LawDiff.tsx
@@ -1,10 +1,11 @@
 import { Typography } from 'antd';
 import { LawEditor } from 'law-document';
-import { FC } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { Descendant } from 'slate';
 import { useSlate } from 'slate-react';
+import { useDebounce } from '@uidotdev/usehooks';
 import useLanguageContext, { Translator } from '../App/useLanguageContext';
-import { Changelog, diffLaw } from './utils/diffLaw';
+import { diffLaw, LawDiffResult } from './utils/diffLaw';
 
 const { Text } = Typography;
 
@@ -13,68 +14,60 @@ interface Props {
     originalDocument: Descendant[];
 }
 
-
 const LawDiff: FC<Props> = ({ originalDocument }) => {
     const slate = useSlate();
     const { t } = useLanguageContext();
-    const changelog = diffLaw(slate, originalDocument);
+    const [diff, setDiff] = useState<LawDiffResult[]>([]);
+    const debouncedChildren = useDebounce(slate.children, 250);
 
-    if (changelog.length === 0) {
+    useEffect(() => {
+        void diffLaw({ children: debouncedChildren } as LawEditor, originalDocument).then(setDiff);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [debouncedChildren]);
+
+    if (diff.length === 0) {
         return (
             <span>{t('No changes')}</span>
         );
     }
 
-    const groupedChanges = groupChangesByArticle(changelog);
-    const renderedChanges = Object.entries(groupedChanges).map(([id, changes], index) => {
+    const changes = diff.map(({ id, changes }, index) => {
         return (
             <div key={`${id}-${index}`} style={{ position: 'relative' }}>
                 <div>{parseIdToDisplay(t, id)}</div>
                 <div>
                     <ol type='a'>
-                        {changes.map((change) => (
-                            <LawChange key={`${id}-${index}-${change.id}`} entry={change} />
-                        ))}
+                        <LawChanges key={`${id}-${index}`} changes={changes ?? []} />
                     </ol>
                 </div>
             </div>
         );
+        
     });
 
     return (
         <div>
-            {renderedChanges}
+            {changes}
         </div>
     );
 };
 
-const groupChangesByArticle = (changelog: Changelog[]) => {
-    const changes: Record<string, Changelog[]> = {};
-
-    changelog.forEach(change => {
-        const articleId = change.id.split('.')[1] ?? change.id;
-
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (!changes[articleId]) {
-            changes[articleId] = [];
-        }
-
-        changes[articleId].push(change);
-    });
-
-    return changes;
-};
-
-const LawChange: FC<{ entry: Changelog }> = ({ entry }) => {
-    const { t } = useLanguageContext();
-
-    return (
+const LawChanges: FC<{ changes: [type: number, text: string][] }> = ({ changes }) => {
+    return useMemo(() => (
         <div>
-            <li key={entry.id}>
-                {parseChange(t, entry)}
-            </li>
+            {changes.map(([type, value], index) => {
+                if (type === 1) {
+                    return <Text key={`${value} / ${index} / ${type}`} type="success" strong>{value}</Text>;
+                }
+
+                if (type === -1) {
+                    return <Text key={`${value} / ${index}`} type="danger" delete strong>{value}</Text>;
+                }
+
+                return <Text key={`${value} / ${index}`}>{value}</Text>;
+            })}
         </div>
-    );
+    ), [changes]);
 };
 
 const parseIdToDisplay = (t: Translator, id: string) => {
@@ -83,48 +76,6 @@ const parseIdToDisplay = (t: Translator, id: string) => {
         .map(([type, nr]) => nr ? `${nr}. ${t(type)}.` : `${t(type)}`)
         .reverse()
         .join(' ');
-};
-
-const parseChange = (t: Translator, entry: Changelog) => {
-    const id = parseIdToDisplay(t, entry.id);
-
-    if (entry.type === 'added') {
-        return `${id} ${t('of the law was added')}: ${entry.text}`;
-    }
-
-    if (entry.type === 'deleted') {
-        return `${id} ${t('of the law was removed.')}`;
-    }
-
-    if (!entry.changes) {
-        return `${id} ${t('of the law shall be')}: ${entry.text}`;
-    }
-
-    return (
-        <>
-            {id} {t('of the law shall be')}: {embedChangesToText(entry.changes)}
-        </>
-    );
-
-    return '';
-};
-
-const embedChangesToText = (changes: NonNullable<Changelog['changes']>) => {
-    return (
-        <span>
-            {changes.map(([type, value]) => {
-                if (type === 1) {
-                    return <Text key={value} type="success" strong>{value}</Text>;
-                }
-
-                if (type === -1) {
-                    return <Text key={value} type="danger" delete strong>{value}</Text>;
-                }
-
-                return <Text key={value}>{value}</Text>;
-            })}
-        </span>
-    );
 };
 
 export default LawDiff;
--- client/src/features/Editor/utils/diffLaw.tsx
+++ client/src/features/Editor/utils/diffLaw.tsx
@@ -1,100 +1,143 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
+import { diffAsXml } from 'diff-js-xml';
+import { exportXml } from 'law-document';
 import { Descendant, Editor } from 'slate';
 import Diff from 'text-diff';
-import { FlattenedParagraph, flattenSlateParagraphs } from './flattenSlateParagraphs';
-
-export interface Changelog {
-    /** The id of the paragraph, Eg.: chapter-1.art-1 */
-    id: string;
-    text?: string;
-    type: 'added' | 'changed' | 'deleted';
-    changes?: [type: number, text: string][];
-}
 
 const diff = new Diff();
 
-export const diffLaw = (editor: Editor, original: Descendant[]) => {
-    const originalTexts = flattenSlateParagraphs(original);
-    const newTexts = flattenSlateParagraphs(editor.children);
-
-    let changelog = createChangelog(originalTexts, newTexts);
-    changelog = sortChangelog(changelog);
-    
-    return changelog;
+type XmlDiffResult = {
+    path: string; // Example: "law.art[1].subart.sen"
+    resultType: string; // Example: "difference in element value"
+    message: string; 
 };
 
-const createChangelog = (originalTexts: FlattenedParagraph[], newTexts: FlattenedParagraph[]) => {
-    const changelog: Changelog[] = [];
-    
-    for (const text of newTexts) {
-        const original = originalTexts.find(original => original.id === text.id);
+export type LawDiffResult = {
+    id: string;
+    texts: string[];
+    originalTexts: string[];
+    type: 'added' | 'changed' | 'deleted';
+    changes?: [type: number, text: string][];
+};
 
-        if (!original) {
-            parseAdded(changelog, text);
+export const diffLaw = async (editor: Editor, original: Descendant[]) => {
+    const newXml = exportXml(editor);
+    const oldXml = exportXml({ children: original } as Editor);
+
+    const results = await new Promise<XmlDiffResult[]>(resolve => 
+        diffAsXml(oldXml, newXml, undefined, undefined, resolve)
+    );
+
+    return parseResults(results);
+};
+
+const parseResults = (results: XmlDiffResult[]) => {
+    const mergedResults: LawDiffResult[] = [];
+
+    for (const result of results) {
+        const { path, message } = result;
+        const id = convertPathToId(path);
+        const { lhs, rhs } = convertMessage(path, message);
+        const texts = deepExtractText(rhs);
+        const originalTexts = deepExtractText(lhs);
+
+        const parsedResult: LawDiffResult = {
+            id,
+            texts,
+            originalTexts,
+            type: 'changed' as const,
+            changes: getTextDiff(originalTexts.join(' '), texts.join(' ')),
+        };
+
+        const existingResult = mergedResults.find(existingResult => existingResult.id === id);
+
+        if (existingResult) {
+            existingResult.texts = existingResult.texts.concat(texts);
+            existingResult.originalTexts = existingResult.originalTexts.concat(originalTexts);
+            existingResult.changes = existingResult.changes ? existingResult.changes.concat(parsedResult.changes ?? []) : parsedResult.changes;
         } else {
-            parseChanged(changelog, text, original);
+            mergedResults.push(parsedResult);
         }
     }
 
-    for (const original of originalTexts) {
-        const newText = newTexts.find(newText => newText.id === original.id);
-
-        if (!newText) {
-            parseRemoved(changelog, original);
-        }
-    }
-
-    return changelog;
-};
-
-const parseAdded = (
-    changelog: Changelog[],
-    text: FlattenedParagraph,
-) => {
-    changelog.push({ id: text.id, type: 'added', text: text.content });
-};
-
-const parseRemoved = (
-    changelog: Changelog[],
-    text: FlattenedParagraph,
-) => {
-    changelog.push({ id: text.id, type: 'deleted', text: text.content });
-};
-
-const parseChanged = (
-    changelog: Changelog[],
-    newText: FlattenedParagraph,
-    originalText: FlattenedParagraph,
-) => {
-    const changes = getTextDiff(originalText, newText);
-
-    // Ignore when they are no changes
-    if (changes.length === 1 && changes[0][0] === 0) {
-        return;
-    }
-        
-    if (changes.length === 1) {
-        const type = changes[0][0] === 1 ? 'added' : 'deleted';
-            
-        changelog.push({ id: newText.id, type, text: newText.content, changes });
-    } else {
-        changelog.push({ id: newText.id, type: 'changed', text: newText.content, changes });
-    }
-};
-
-const getTextDiff = (originalText: FlattenedParagraph, newText: FlattenedParagraph) => {
-    const changes = diff.main(originalText.content, newText.content);
-    diff.cleanupSemantic(changes);
-    return changes;
+    return mergedResults;
 };
 
 /**
- * Sort changelog based on id, ascending
- * @param changelog 
- * @param events 
- * @returns 
+ * Converts the path from the xml diff to the id of the paragraph for display
  */
-const sortChangelog = (changelog: Changelog[]) => {
-    return [...changelog]
-        .sort((a, b) => a.id < b.id ? -1 : a.id === b.id ? 0 : 1);
+const convertPathToId = (path: string) => {
+    let newPath = path.split('.')
+        .filter(level => !['law', 'name', 'nr-title'].includes(level))
+        .map(level => {
+            return level
+                .replace(/\[/g, '-')
+                .replace(/\]/g, '')
+                .split('-');
+        });
+
+    // Remove the last element because for the paragraphId it should point at the enclosing node, 
+    // and the xml-diff returns the siblings of the targeted path
+    if (newPath.length > 1) {
+        newPath = newPath.slice(0, -1);
+    }
+        
+    return newPath
+        .map(([type, nr]) => `${type}-${parseInt(nr ?? 0) + 1}`)
+        .join('.');
+};
+
+/**
+ * Converts the message from the xml diff to json for easier handling
+ */
+const convertMessage = (path: string, message: string) => {
+    const [lhs, rhs] = message
+        .replace(`field ${path} has lhs value`, '')
+        .split(' and rhs value ')
+        .map<unknown>(value => JSON.parse(value));
+
+    return {
+        lhs,
+        rhs,
+    };
+};
+
+/**
+ * Extracts text from deep json and returns it as a array of strings
+ **/
+const deepExtractText = (json: unknown): string[] => {
+    if (!json) {
+        return [];
+    }
+
+    if (typeof json === 'string') {
+        return [json.trim()];
+    }
+
+    if (Array.isArray(json)) {
+        // TODO: Why?
+        if (json.length > 1 && typeof json[0] === 'string') {
+            return [json.map(s => s.trim()).join(' ')];
+        }
+
+        return json.flatMap(deepExtractText);
+    }
+
+    if (typeof json === 'object') {
+        return Object.values(json).flatMap(deepExtractText);
+    }
+
+    return [];
+};
+
+/**
+ * Returns the diff between two strings as an array of changes
+ * - 0: no change
+ * - 1: added
+ * - -1: removed
+ */
+const getTextDiff = (originalText: string, newText: string) => {
+    const changes = diff.main(originalText, newText);
+    diff.cleanupSemantic(changes);
+    return changes;
 };
