4b91a0e393e875b1c12918907cf282c637c30416
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-04-23 14:55:33 +0700

    Fix type issue with LawEditor not matching DOMEditor

--- law-document-editor/src/Editor/slate.d.ts
+++ law-document-editor/src/Editor/slate.d.ts
@@ -1,9 +1,8 @@
 import { LawEditor, LawElement, TextNode } from 'law-document';
-import { ReactEditor } from 'slate-react';
+import { DOMEditor, ReactEditor } from 'slate-react';
 
 declare module 'law-document' {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    interface LawEditor extends ReactEditor {}
+    interface LawEditor extends ReactEditor, DOMEditor {}
 }
 
 declare module 'slate' {
