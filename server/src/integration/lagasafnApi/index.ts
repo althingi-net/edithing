fb3b4160101844cc59122587c13802d46dc5b9ef
GHaralds <g@althingi.net>
2025-05-05 16:20:49 +0000

    Correct URL to validation endpoint

--- server/src/integration/lagasafnApi/index.ts
+++ server/src/integration/lagasafnApi/index.ts
@@ -3,12 +3,12 @@ import { lagasafnApi } from '../../config/lagasafnApi';
 
 export const postBillForValidation = async (billXml: string) => {
     try {
-        await axios.post(lagasafnApi.url, billXml);
+        await axios.post(lagasafnApi.url + '/api/bill/validate', billXml);
     } catch (error) {
         console.log('Request to lagasafn failed', error);
     }
 };
 
 export const postBillForPublishing = ( billXml: string) => {
-    console.log('Bill XML published: ' + billXml);
+    console.log('Bill XML published!');
 };
