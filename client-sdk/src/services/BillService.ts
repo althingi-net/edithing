263248479777a0c474d93f4cb071ad3e1e7cf886
GHaralds <g@althingi.net>
2025-05-05 13:13:02 +0000

    Add API logic to publish bill XML

--- client-sdk/src/services/BillService.ts
+++ client-sdk/src/services/BillService.ts
@@ -89,4 +89,21 @@ export class BillService {
             },
         });
     }
+    /**
+     * Publish bill XML.
+     * @param id
+     * @returns any Successful response
+     * @throws ApiError
+     */
+    public static billControllerPublishXml(
+        id: number,
+    ): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/bills/{id}/xml',
+            path: {
+                'id': id,
+            },
+        });
+    }
 }
