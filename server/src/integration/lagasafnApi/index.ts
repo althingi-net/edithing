409ba5f129f966d307b17e0385274b249d57931e
GHaralds <g@althingi.net>
2025-05-06 16:45:03 +0000

    Add logic to publish bill

--- server/src/integration/lagasafnApi/index.ts
+++ server/src/integration/lagasafnApi/index.ts
@@ -7,10 +7,18 @@ export const postBillForValidation = async (billXml: string) => {
     try {
         await axios.post(lagasafnApi.url + '/api/bill/validate', billXml);
     } catch (error) {
+        // FIXME: Let user know of error.
         console.log('Request to lagasafn failed', error);
     }
+    console.log('Bill validated!');
 };
 
-export const postBillForPublishing = ( billXml: string) => {
+export const postBillForPublishing = async (billXml: string) => {
+    try {
+        await axios.post(lagasafnApi.url + '/api/bill/publish', billXml);
+    } catch (error) {
+        // FIXME: Let user know of error.
+        console.log('Request to lagasafn failed', error);
+    }
     console.log('Bill XML published!');
 };
