47dd2cb2d25efc32712603c98cf0684f0fb00926
GHaralds <g@althingi.net>
2025-05-08 15:49:15 +0000

    Catch error, parse and re-throw

--- server/src/integration/lagasafnApi/index.ts
+++ server/src/integration/lagasafnApi/index.ts
@@ -6,19 +6,15 @@ axios.defaults.headers.post['Content-Type'] = 'application/xml';
 export const postBillForValidation = async (billXml: string) => {
     try {
         await axios.post(lagasafnApi.url + '/api/bill/validate', billXml);
-    } catch (error) {
-        // FIXME: Let user know of error.
-        console.log('Request to lagasafn failed', error);
+    } catch (error: any) {
+        throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) )
     }
-    console.log('Bill validated!');
 };
 
 export const postBillForPublishing = async (billXml: string) => {
     try {
         await axios.post(lagasafnApi.url + '/api/bill/publish', billXml);
-    } catch (error) {
-        // FIXME: Let user know of error.
-        console.log('Request to lagasafn failed', error);
+    } catch (error: any) {
+        throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) )
     }
-    console.log('Bill XML published!');
 };
