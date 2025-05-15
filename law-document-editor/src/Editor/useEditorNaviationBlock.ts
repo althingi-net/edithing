43bd539f56aba3a81481da494b69d56d6571c09f
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-05-15 13:30:03 +0700

    Add documentation to useEditorNavigationBlock

--- law-document-editor/src/Editor/useEditorNaviationBlock.ts
+++ law-document-editor/src/Editor/useEditorNaviationBlock.ts
@@ -8,6 +8,10 @@ export interface NavigationBlocker {
     goTo: (path: string) => void;
 }
 
+/**
+ * This hook is used to block navigation when the editor is saving.
+ * It will block navigation when the editor has unsaved changes and unblock it when the changes are saved.
+ */
 export const useEditorNavigationBlock = (editor: LawEditor, navigationBlocker: NavigationBlocker, saveDocument?: (editor: LawEditor) => void) => {
     const { isNavigationBlocked, blockNavigation, unblockNavigation } = navigationBlocker;
 
