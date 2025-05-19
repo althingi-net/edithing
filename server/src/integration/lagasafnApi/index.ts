435914cd9b41b7ba0f7de4bcb341b91ef308306c
GHaralds <g@althingi.net>
2025-05-19 16:44:13 +0000

    Use bill ID when publishing bill

--- server/src/integration/lagasafnApi/index.ts
+++ server/src/integration/lagasafnApi/index.ts
@@ -3,6 +3,14 @@ import { lagasafnApi } from '../../config/lagasafnApi';
 
 axios.defaults.headers.post['Content-Type'] = 'application/xml';
 
+export const postBillMeta = async (billMetaXml: string) => {
+    try {
+        await axios.post(lagasafnApi.url + '/api/bill/meta', billMetaXml);
+    } catch (error: any) {
+        throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) );
+    }
+};
+
 export const postBillForValidation = async (billXml: string) => {
     try {
         await axios.post(lagasafnApi.url + '/api/bill/document/validate', billXml);
@@ -11,9 +19,9 @@ export const postBillForValidation = async (billXml: string) => {
     }
 };
 
-export const postBillForPublishing = async (billXml: string) => {
+export const postBillForPublishing = async (billNr: number, billXml: string) => {
     try {
-        await axios.post(lagasafnApi.url + '/api/bill/document/publish', billXml);
+        await axios.post(lagasafnApi.url + '/api/bill/' + String( billNr ) + '/document/publish', billXml);
     } catch (error: any) {
         throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) );
     }
