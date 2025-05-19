c47255b8430e49ade933b0a9961d11dd220c4e29
GHaralds <g@althingi.net>
2025-05-19 16:50:49 +0000

    Add lagasafnID and description fields

--- law-document/src/slate/element/TextNode.ts
+++ law-document/src/slate/element/TextNode.ts
@@ -1,13 +1,19 @@
 import { Node, Text } from 'slate';
 
 export interface TextNode {
+    lagasafnID?: string;
     text: string;
     title?: boolean;
     name?: boolean;
+    description?: string;
     nr?: string;
     bold?: boolean;
     expirySymbolOffset?: string;
 }
+
+export const isPublicID = (node?: Node | null): node is Text => {
+    return Text.isText(node) && 'number' === typeof node.lagasafnID;
+};
 
 export const isName = (node?: Node | null): node is Text => {
     return Text.isText(node) && Boolean(node.name);
@@ -15,4 +21,8 @@ export const isName = (node?: Node | null): node is Text => {
 
 export const isTitle = (node?: Node | null): node is Text => {
     return Text.isText(node) && Boolean(node.title);
+};
+
+export const isDescription = (node?: Node | null): node is Text => {
+   return Text.isText(node) && Boolean(node.description);
 };
