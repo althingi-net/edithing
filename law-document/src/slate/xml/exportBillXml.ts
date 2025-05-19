200f3e84f8db02e934b24c2b92bbf8a4eed9f761
GHaralds <g@althingi.net>
2025-05-19 16:49:50 +0000

    Add bill meta XML export, change bill export function

--- law-document/src/slate/xml/exportBillXml.ts
+++ law-document/src/slate/xml/exportBillXml.ts
@@ -6,17 +6,31 @@ export interface BillDocument {
     originalXml: string;
     content: string;
     identifier: string;
-    title: string;
 }
 
-export const exportBillXml = (billTitle: string, billDocuments: BillDocument[]): string => {
+export const exportBillMetaXml = (billPublicID: number, billTitle: string, billDescription: string): string => {
+
+    return xmlFormat(`
+        <bill>
+            <lagasafnID>
+                ${billPublicID}
+            </lagasafnID>
+            <title>
+                ${billTitle}
+            </title>
+            <description>
+                ${billDescription}
+            </description>
+        </bill>
+    `);
+};
+
+
+export const exportBillXml = (billDocuments: BillDocument[]): string => {
     const documents = billDocuments.map(document => parseDocument(document));
 
     return xmlFormat(`
         <bill>
-            <title>
-                ${billTitle}
-            </title>
             ${documents.join('\n')}
         </bill>
     `);
