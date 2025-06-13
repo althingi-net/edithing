2bebbe5e2b08073fa25985d5422d9e7ee9d009c0
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-06-13 10:32:31 +0700

    Remove usage of global axios configuration

--- server/src/integration/lagasafnApi/index.ts
+++ server/src/integration/lagasafnApi/index.ts
@@ -1,28 +1,40 @@
 import axios from 'axios';
 import { lagasafnApi } from '../../config/lagasafnApi';
 
-axios.defaults.headers.post['Content-Type'] = 'application/xml';
-axios.defaults.headers.post['Authorization'] = 'Bearer ' + lagasafnApi.api_access_token;
+const getConfig = () => {
+    return {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Authorization': 'Bearer ' + lagasafnApi.api_access_token,
+        },
+    };
+};
 
 export const postBillMeta = async (billMetaXml: string) => {
+    const url = lagasafnApi.url + '/api/bill/meta';
+
     try {
-        await axios.post(lagasafnApi.url + '/api/bill/meta', billMetaXml);
+        await axios.post(url, billMetaXml, getConfig());
     } catch (error: any) {
         throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) );
     }
 };
 
 export const postBillForValidation = async (billXml: string) => {
+    const url = lagasafnApi.url + '/api/bill/document/validate';
+
     try {
-        await axios.post(lagasafnApi.url + '/api/bill/document/validate', billXml);
+        await axios.post(url, billXml, getConfig());
     } catch (error: any) {
         throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) );
     }
 };
 
 export const postBillForPublishing = async (billNr: number, billXml: string) => {
+    const url = lagasafnApi.url + '/api/bill/' + String( billNr ) + '/document/publish';
+
     try {
-        await axios.post(lagasafnApi.url + '/api/bill/' + String( billNr ) + '/document/publish', billXml);
+        await axios.post(url, billXml, getConfig());
     } catch (error: any) {
         throw new Error( <string>( error?.response.data?.message ?? 'Unknown error' ) );
     }
